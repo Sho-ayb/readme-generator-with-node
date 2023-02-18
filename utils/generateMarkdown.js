4a7d13338c536be662d470fb4714a2272c826919
Sho.ayb <Sho.ayb@outlook.com>
2023-02-18 23:05:11 +0000

    added a switch case to generateMarkdown function for adding the correct licence badge in readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -24,8 +24,34 @@ function generateMarkdown(data) {
     emailaddress
   );
 
+  let licenceBadge;
+
+  switch (licence) {
+    case "Apache License 2.0":
+      licenceBadge =
+        "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "GPL License (aka GNU General Public License v3.0)":
+      licenceBadge =
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "BSD License":
+      licenceBadge =
+        "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+      break;
+    case "The Unlicense":
+      licenceBadge =
+        "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+      break;
+    default:
+      licenceBadge =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  }
+
   const markdown = `
-TABLE OF CONTENTS 
+${licenceBadge}
+
+# TABLE OF CONTENTS 
 
 - [Project Title](#project-title)
 - [Description](#description)
@@ -79,6 +105,9 @@ ${emailaddress}
 # Licence
 
 ${licence}
+
+Please click on the badge for more details on the licence.
+${licenceBadge}
 
 `;
 
